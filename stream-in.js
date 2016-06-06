73d140148d613140047881769a790aea4001024e
Pedro Maia <pedrommone@gmail.com>
2016-06-06 20:12:04 -0300

    Remove commented codes

--- stream-in.js
+++ stream-in.js
@@ -1,3 +1,7 @@
+// docker run --name redis --rm redis:3.2.0
+// docker run -it --link redis:redis --rm redis:3.2.0 redis-cli -h redis -p 6379
+// docker run -it --link redis:redis --volume ~/Desktop/LiveRedis/:/app --rm node:6.2.0 node /app/stream-in.js
+
 var redis = require("redis");
 var readline = require('readline');
 var bluebird = require('bluebird');
@@ -7,26 +11,19 @@ bluebird.promisifyAll(redis.RedisClient.prototype);
 var sub = redis.createClient();
 var rl = readline.createInterface(process.stdin, process.stdout);
 
+rl.setPrompt('digite> ');
+rl.prompt();
 
-// sub.on('subscribe', function (channel, count) {
-//
-//
-    rl.setPrompt('guess> ');
-    rl.prompt();
+rl.on('line', line => {
+    if (line === "quit") rl.close();
 
-    rl.on('line', line => {
-        if (line === "quit") rl.close();
+    sub.getAsync(`chave-${line}`)
+      .then(data => {
+        console.log(data);
+        rl.prompt();
+      })
+      .catch(err => console.log(err));
 
-        sub.getAsync(`chave-${line}`)
-          .then(data => {
-            console.log(data);
-            rl.prompt();
-          })
-          .catch(err => console.log(err));
-
-    }).on('close', () => {
-        //sub.quit();
-        process.exit(0);
-    });
-//
-// });
+}).on('close', () => {
+    process.exit(0);
+});
