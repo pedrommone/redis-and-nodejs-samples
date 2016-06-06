f8d72e3ff8470fe7d30a84117516109a4ac2d17e
marciomansur <mrabeloo@gmail.com>
2016-06-06 09:15:43 -0300

    changed some lines for async code (still need some fix)

--- mark.js
+++ mark.js
@@ -1,23 +1,28 @@
-// docker run --name redis --rm redis:3.2.0 
+// docker run --name redis --rm redis:3.2.0
 // docker run -it --link redis:redis --rm redis:3.2.0 redis-cli -h redis -p 6379
 // docker run -it --link redis:redis --volume ~/Desktop/LiveRedis/:/app --rm node:6.2.0 node /app/mark.js
 
 var redis = require("redis"),
+    bluebird = require('bluebird'),
     total = 100000;
-    client = redis.createClient({
-        "host": "redis",
-    });
+
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+
+client = redis.createClient();
 
 console.log(`Escrita e leitura de ${total} objetos`);
 
-client.on("error", function (err) {
-    console.log(`Error ${err}`);
-});
+client.on("error", (err) => console.log(`Error ${err}`));
 
 var startUnitSet = new Date().getTime();
 
 for (i = 0; i < total; ++i) {
-    client.set(`chave-${i}`, `valor-${i}`);
+    client.setAsync(`chave-${i}`, `valor-${i}`)
+      .then(data =>
+        console.log(data)
+      )
+      .catch(err => console.log(err));
 }
 
 var endUnitSet = new Date().getTime();
@@ -28,7 +33,7 @@ console.log(`Tempo de execucao de escrita: ${timeUnitSet}ms`);
 var startUnitGet = new Date().getTime();
 
 for (j = 0; j < total; ++j) {
-    client.get(`chave-${j}`);
+
 }
 
 var endUnitGet = new Date().getTime();
@@ -36,4 +41,6 @@ var timeUnitGet = endUnitGet - startUnitGet;
 
 console.log(`Tempo de execucao de leitura: ${timeUnitGet}ms`);
 
-client.quit();
+// client.quit();
+
+// process.exit();
